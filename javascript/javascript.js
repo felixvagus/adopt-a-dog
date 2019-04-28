816a82afc27a8ed5e2451e05df55b4d0348b6da6
Daniel Kiefer <royen9880@msn.com>
2019-04-28 11:08:38 -0700

    started JS button and removed css from index page

---
+++ javascript/javascript.js
@@ -0,0 +1,12 @@
+function murphyInfo() {
+  let input = prompt('Murphy, good boi, cost: $100.00');
+  let result = 0;
+
+  alert(result);
+}
+
+function murphyPurchase() {
+  let input = prompt('');
+  let numberToAdd = 100;
+  let result =
+}
