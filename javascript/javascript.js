a0ada2fa86dc6793d36d7eee1f636f6e4d7dfcde
Daniel Kiefer <royen9880@msn.com>
2019-04-29 20:18:56 -0700

    finally have functioning code for lesson04 assignment 2

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,21 @@
 function murphyInfo() {
-  let = prompt("Murphy, good boi, cost: $100.00");
-  alert(prompt);
-}
+  let myPrompt = "Murphy, good boi, cost: $100.00";
+  alert(myPrompt);
+};
 
 function murphyPurchase() {
-  let input = prompt('');
-  let numberToAdd = 100;
-  let result =
+total = 0;
+total += frm.contact_plugin.value * 10 + frm.mailing_list.value * 10;
+document.getElementById("total").innerHTML = total;
+}
+
+//const input = document.getElementById('myInput');
+//input.value = '0';
+let addCart =0;
+
+function buttonClick(event){
+   // var startNumber = parseInt(input.value, 10);
+    addCart = addCart + 10;
+    alert(addCart);
+    //input.value = startNumber;
 }
