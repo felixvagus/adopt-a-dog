a0d7b78b0e81ec53d71c487e69c98420f010d54a
Daniel Kiefer <royen9880@msn.com>
2019-04-29 20:27:44 -0700

    fixed the rest of the buttons

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,13 +9,9 @@ total += frm.contact_plugin.value * 10 + frm.mailing_list.value * 10;
 document.getElementById("total").innerHTML = total;
 }
 
-//const input = document.getElementById('myInput');
-//input.value = '0';
 let addCart =0;
 
 function buttonClick(event){
-   // var startNumber = parseInt(input.value, 10);
-    addCart = addCart + 10;
+    addCart = addCart + 100.00;
     alert(addCart);
-    //input.value = startNumber;
 }
